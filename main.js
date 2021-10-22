fb28463ce6c4e7c169be5cee48cfa4857eafd571
Luiz Gustavo Falqueto <lgfalqueto85@gmail.com>
2021-10-21 21:36:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+const linksSocialMedia = {
+  github: 'luizgfalqueto',
+  facebook: 'luizgfalqueto',
+  youtube: 'UCze5CUGDNt3hms6Co7TjChQ',
+  twitter: 'luizgfalqueto',
+  instagram: 'luizgfalqueto'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    if (social == 'youtube') {
+      li.children[0].href = `https://${social}.com/channel/${linksSocialMedia[social]}`
+    } else {
+      li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+    }
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
